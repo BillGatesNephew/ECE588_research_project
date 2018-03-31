f6c65292aab30423dd1a01175ba9d2f94a3ad849
BillGatesNephew <wlkrwilletts@gmail.com>
2018-03-31 15:26:33 -0400

    Making the script better.

--- scripts/keyDetection.py
+++ scripts/keyDetection.py
@@ -26,6 +26,8 @@ KEY_MAP = [
 videoName = 'typing_vid.mp4'
 # Threshold for how far a square can be off a baseline to be on a different row
 rowBaselineThreshold = 30
+# Number of final frame windows to show
+frameNumber = 4
 
 ## Contour Detection Parameters
 # Maximum valid size needed for a detected contour to be a key
@@ -81,93 +83,131 @@ def center_y_position(square):
 ## Actual Keyboard Detection Script ##
 #########################################################################
 
+
+def get_key_contours(frame):
+    # Rotate to proper orientation
+    rotatedFrame = rotate_image(frame, 90)
+    # Resize image to mainly contain only the keyboard
+    rows,cols,_ = rotatedFrame.shape
+    rowShift = (rows * 2) // 3
+    resizedImage = rotatedFrame[rowShift : rows]
+    # Convert to image to grayscale and apply binary thresholding
+    grayImage = cv2.cvtColor(resizedImage, cv2.COLOR_BGR2GRAY)
+    _, binaryImage = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY_INV)
+
+    ## Detect Squares in Image ##
+    # Apply canny edge detection, and dilate results
+    cannyImage = cv2.Canny(binaryImage, 0, 50, apertureSize=5)
+    dilatedCannyImage = cv2.dilate(cannyImage, None)
+    # Find contours within the dilated image
+    _, contours, _ = cv2.findContours(dilatedCannyImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # Run square detection algorithm
+    squares = []
+    centerPositions = []
+    for cnt in contours:
+        # Approximate a polynomial for contour
+        cnt_len = 0.02*cv2.arcLength(cnt, True)
+        cnt = cv2.approxPolyDP(cnt, cnt_len, True)
+        cntArea = cv2.contourArea(cnt)
+        # Determine whether to keep contour or not
+        if len(cnt) == 4 and cntArea > minImageKeyArea and cntArea < maxImageKeyArea and cv2.isContourConvex(cnt):
+            cnt = cnt.reshape(-1, 2)
+            center = (center_x_position(cnt), center_y_position(cnt))
+            # Check to make sure a similar contour hasn't been added
+            addToLists = 1
+            for (index, centerPosition) in enumerate(centerPositions):
+                distance = calculate_distance(centerPosition, center)
+                oldArea = cv2.contourArea(squares[index])
+                # Replace old square with larger square if possible
+                if distance < distanceThreshold and cntArea > oldArea:
+                    squares.pop(index)
+                    centerPositions.pop(index)
+                elif distance < distanceThreshold:
+                    addToLists = 0
+            if addToLists:
+                # Shift down for original image and then append to list
+                for corner in cnt:
+                    corner[1] += rowShift
+                squares.append(cnt)
+                centerPositions.append(center)
+
+    ## Divide Keys Detected into Rows ##
+    # Sort by square center Y-coordinate to get keys ordered into "pseudo-rows" 
+    squares.sort(key=center_y_position, reverse=False)
+    # Divide square centered around common baseline into rows representing the keyboard
+    rows = [[]]
+    currentRow = 0
+    currentRowBaseY = center_y_position(squares[0])
+    for square in squares:
+        currSquareY = center_y_position(square)
+        if np.abs(currSquareY - currentRowBaseY) > rowBaselineThreshold:
+            currentRowBaseY = currSquareY
+            rows.append([])
+            currentRow = currentRow + 1
+        rows[currentRow].append(square)
+        rows[currentRow].sort(key=center_x_position, reverse=False)
+    # Combine rows back together in proper order
+    squares = []
+    for row in rows:
+        squares += row
+    #squares = rows[0] + rows[1] + rows[2] + rows[3] + rows[4]
+    if len(rows[0]) != 14:
+        squares.reverse()
+    return squares
+
+
+def draw_squares(frame, squares):
+    rotatedFrame = rotate_image(frame, 90)
+    ## Draw The Keys with Labels and Create Location Map ##
+    for key in range(0, len(squares)):
+        if len(squares) > 61:
+            break
+        square = squares[key]
+        # Calculate Key Centers
+        center = (center_x_position(square), center_y_position(square))
+        # Add Key to Map
+        # Caclulate Text Position
+        textSize, _ = cv2.getTextSize(KEY_MAP[key],fontFamily, fontSize,fontThickness)
+        textXposition = int(center[0] - textSize[0] / 2)
+        textYposition = int(center[1] + textSize[1] / 2)
+        # Draw Squares and Text on Image
+        cv2.drawContours(rotatedFrame, [square], -1, (0,255,0), 2)
+        cv2.putText(rotatedFrame, KEY_MAP[key], (textXposition, textYposition), fontFamily, fontSize, (0, 0, 255), fontThickness, cv2.LINE_AA)
+    return rotatedFrame
+
+def adjust_squares(currentFrameSquares, squares):
+    print(len(squares))
+    print(len(currentFrameSquares))
+    if len(squares) < 61:
+        return currentFrameSquares
+    if len(currentFrameSquares) < 61:
+        return squares
+    for key in range(0, len(currentFrameSquares)):
+        square = currentFrameSquares[key]
+        newCenter = (center_x_position(square), center_y_position(square))
+        oldSquare = squares[key]
+        oldCenter = (center_x_position(oldSquare), center_y_position(oldSquare))
+        if calculate_distance(newCenter, oldCenter) > 10:
+            print(calculate_distance(newCenter, oldCenter))
+            squares[key] = square
+    return squares
+
 ## Load and Adjust Initial Video Frame ##
-# Load in frame
+# Load in video
 cap = cv2.VideoCapture(videoName)
-_,frame = cap.read()
-# Rotate to proper orientation
-rotatedFrame = rotate_image(frame, 90)
-# Resize image to mainly contain only the keyboard
-rows,cols,_ = rotatedFrame.shape
-rowShift = (rows * 2) // 3
-resizedImage = rotatedFrame[rowShift : rows]
-# Convert to image to grayscale and apply binary thresholding
-grayImage = cv2.cvtColor(resizedImage, cv2.COLOR_BGR2GRAY)
-_, binaryImage = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY_INV)
 
-## Detect Squares in Image ##
-# Apply canny edge detection, and dilate results
-cannyImage = cv2.Canny(binaryImage, 0, 50, apertureSize=5)
-dilatedCannyImage = cv2.dilate(cannyImage, None)
-# Find contours within the dilated image
-_, contours, _ = cv2.findContours(dilatedCannyImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-# Run square detection algorithm
+# Play with detected keys
 squares = []
-centerPositions = []
-for cnt in contours:
-    # Approximate a polynomial for contour
-    cnt_len = 0.02*cv2.arcLength(cnt, True)
-    cnt = cv2.approxPolyDP(cnt, cnt_len, True)
-    cntArea = cv2.contourArea(cnt)
-    # Determine whether to keep contour or not
-    if len(cnt) == 4 and cntArea > minImageKeyArea and cntArea < maxImageKeyArea and cv2.isContourConvex(cnt):
-        cnt = cnt.reshape(-1, 2)
-        center = (center_x_position(cnt), center_y_position(cnt))
-        # Check to make sure a similar contour hasn't been added
-        addToLists = 1
-        for (index, centerPosition) in enumerate(centerPositions):
-            distance = calculate_distance(centerPosition, center)
-            oldArea = cv2.contourArea(squares[index])
-            # Replace old square with larger square if possible
-            if distance < distanceThreshold and cntArea > oldArea:
-                squares.pop(index)
-                centerPositions.pop(index)
-            elif distance < distanceThreshold:
-                addToLists = 0
-        if addToLists:
-            # Shift down for original image and then append to list
-            for corner in cnt:
-                corner[1] += rowShift
-            squares.append(cnt)
-            centerPositions.append(center)
+while(True):
+    _,frame = cap.read()
+    currentFrameSquares = get_key_contours(frame)
+    squares = adjust_squares(currentFrameSquares, squares)
+    imageWithSquares = draw_squares(frame, squares)
+    cv2.imshow('frame', imageWithSquares)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
 
-## Divide Keys Detected into Rows ##
-# Sort by square center Y-coordinate to get keys ordered into "pseudo-rows" 
-squares.sort(key=center_y_position, reverse=False)
-# Divide square centered around common baseline into rows representing the keyboard
-rows = [[]]
-currentRow = 0
-currentRowBaseY = center_y_position(squares[0])
-for square in squares:
-    currSquareY = center_y_position(square)
-    if np.abs(currSquareY - currentRowBaseY) > rowBaselineThreshold:
-        currentRowBaseY = currSquareY
-        rows.append([])
-        currentRow = currentRow + 1
-    rows[currentRow].append(square)
-    rows[currentRow].sort(key=center_x_position, reverse=False)
-# Combine rows back together in proper order
-squares = rows[0] + rows[1] + rows[2] + rows[3] + rows[4]
-if len(rows[0]) != 14:
-    squares.reverse()
-
-## Draw The Keys with Labels and Print Areas ##
-for key in range(0, len(squares)):
-    square = squares[key]
-    # Calculate Key Centers
-    center = (center_x_position(square), center_y_position(square))
-    # Print Key Location
-    print("'" + KEY_MAP[key] + "'" + " is at (" + str(center[0]) + ", " + str(center[1]) + ")")
-    # Caclulate Text Position
-    textSize, _ = cv2.getTextSize(KEY_MAP[key],fontFamily, fontSize,fontThickness)
-    textXposition = int(center[0] - textSize[0] / 2)
-    textYposition = int(center[1] + textSize[1] / 2)
-    # Draw Squares and Text on Image
-    cv2.drawContours(rotatedFrame, [square], -1, (0,255,0), 2)
-    cv2.putText(rotatedFrame, KEY_MAP[key], (textXposition, textYposition), fontFamily, fontSize, (0, 0, 255), fontThickness, cv2.LINE_AA)
-
-## Show Final Image Result ##
-cv2.imshow('Original with labeled keys', rotatedFrame)
 if cv2.waitKey(0) & 0xff == 27:
+    cap.release()
     cv2.destroyAllWindows()
 
